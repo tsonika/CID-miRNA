f9205ed57a430b43760935e0963c738cff37e54b
Alejandro Dubrovsky <Alex.Dubrovsky@agrf.org.au>
2015-02-12 13:49:59 +1100

    Split off LocalRunner and Command into a separate runner.py module Fix local calls so that they don't consume a lot of memory keeping stdout when we are not going to use it Fix local multi runs where parameters for one command were influencing the parameters for other calls in a multi-run

--- cidmirna.py
+++ cidmirna.py
@@ -15,6 +15,7 @@ import time
 from mirnastructure import diagramFromStructure
 from foldingsubsequences import DefaultMinLength, DefaultMaxLength, DefaultEndBasePairs
 from utils import generateRNACombinations
+from runner import LocalRunner, Command
 
 DefaultUpperDGCutoff = -13.559
 DefaultGrammarScoreCutoff = -0.609999
@@ -59,178 +60,6 @@ NeedPreload = ['newcyk2']
 #
 #--------------------------------------------------------------------------------------------------------------------------
 
-class Command(list):
-    """
-    Wafer-thin wrapper around a list so that we can specify an output, an error and an input
-    """
-    def __init__(self, *args, **kwargs):
-        output = kwargs.pop('output', None)
-        error = kwargs.pop('error', None)
-        input = kwargs.pop('input', None)
-        list.__init__(self, *args, **kwargs)
-        self.input = input
-        self.error = error
-        self.output = output
-
-
-
-class StandardRunner(object):
-    DefaultPollingPeriod = 30
-
-    def __init__(self, polling_period=None):
-        if polling_period is not None:
-            self.polling_period = polling_period
-        else:
-            self.polling_period = self.DefaultPollingPeriod
-
-    def run(self, command, output_filename=None, error_filename=None, input_filename=None, environment=None):
-        parameters = {
-            'close_fds' : True
-        }
-
-        output_filename = output_filename or getattr(command, 'output', None)
-        if output_filename:
-            output_file = open(output_filename, 'w')
-            parameters['stdout'] = output_file
-
-        input_filename = input_filename or getattr(command, 'input', None)
-        if input_filename:
-            input_file = open(input_filename)
-            parameters['stdin'] = input_file
-
-        error_filename = error_filename or getattr(command, 'error', None)
-        if error_filename:
-            if error_filename != output_filename:
-                error_file = open(error_filename, 'w')
-            else:
-                error_file = output_file
-            parameters['stderr'] = error_file
-
-        if environment:
-            parameters['env'] = environment
-
-        logging.debug("Running %s" % " ".join(command))
-        exit_code = subprocess.call(command, **parameters)
-        logging.debug("Finished running %s with exit code %s" % (" ".join(command), exit_code))
-
-
-        if input_filename:
-            input_file.close()            
-        if output_filename:
-            output_file.close()
-        if error_filename and error_filename != output_filename:
-            error_file.close()
-
-        return exit_code
-
-
-    def multi_run(self, commands, environment=None, max_simultaneous=None, buffer_size=16384):
-        """
-        Run commands and wait for them to finish
-        """
-        import asyncproc
-
-        parameters = {
-            'close_fds' : True,
-            'stderr' : subprocess.PIPE
-        }
-
-        success = True
-        if environment:
-            parameters['env'] = environment
-
-        if buffer_size is not None:
-            parameters['bufsize'] = buffer_size
-
-        processes = {}
-        while True:
-            finished_in_cycle = False
-
-            while commands and ((max_simultaneous is None) or (len(processes) < max_simultaneous)):
-                # we can generate more
-                command = commands.pop()
-                message_parts = ["Running '%s'" % " ".join(command)]
-
-                output_filename = getattr(command, 'output', None)
-                input_filename = getattr(command, 'input', None)
-                error_filename = getattr(command, 'error', None)
-                
-                if input_filename:
-                    input_file = open(input_filename)
-                    parameters['stdin'] = input_file
-                    message_parts.append(' input from %s' % input_filename)
-                else:
-                    input_file = None
-
-                if output_filename:
-                    output_file = open(output_filename, 'w')
-                    parameters['stdout'] = output_file
-                    message_parts.append(' output to %s' % output_filename)
-                else:
-                    output_file = None
-
-                if error_filename:
-                    message_parts.append(' error to %s' % error_filename)
-                    if error_filename != output_filename:
-                        error_file = open(error_filename, 'w')
-                    else:
-                        error_file = output_file
-
-                    parameters['stderr'] = error_file
-                else:
-                    error_file = None
-
-                logging.info(' '.join(message_parts))
-                process = asyncproc.Process(command, **parameters)
-                processes[process] = (command, input_file, output_file, error_file)
-
-            for process, command_details in processes.items():
-                if command_details[-1] is None:
-                    # if we are not piping stderr to a file, read it and log it
-                    error = process.readerr()
-                    if error:
-                        logging.info("Process %s err: %s" % (process.pid(), error))
-
-                exit_code = process.wait(os.WNOHANG) # check if we are done
-                if exit_code is not None:
-                    command, input_file, output_file, error_file = command_details
-                    if input_file is not None:
-                        input_file.close()
-
-                    if output_file is not None:
-                        output_file.close()
-
-                    if error_file is not None and not error_file.closed:
-                        error_file.close()
-
-                    finished_in_cycle = True
-
-                    del processes[process]
-                    if exit_code != 0:
-                        logger = logging.error
-                        success = False
-                    else:
-                        logger = logging.info
-                    logger("'%s' exited with code %s" % (' '.join(command), exit_code))
-
-            # if there's nothing left, quit
-
-            if not commands and not processes:
-                break
-
-            # if nothing finished, sleep for a while, otherwise go for more
-            if not finished_in_cycle:
-                time.sleep(self.polling_period)
-
-
-        return success
-
-
-    def max_processes(self):
-        import multiprocessing
-        return multiprocessing.cpu_count()
-
-
 
 class Runner(object):
     """
@@ -238,7 +67,7 @@ class Runner(object):
     and where the output should go
     """
 
-    Runner = StandardRunner()
+    Runner = LocalRunner()
     MaxProcesses = None
     OutputDirectory = '.'
 
