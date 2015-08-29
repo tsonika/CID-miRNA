f65982c2c30fd0333a256f8f4ff4601f7195af64
Alejandro Dubrovsky <alito@organicrobot.com>
2015-08-29 21:45:19 +1000

    indent -i4 -nut -br -ce -npsl some more

--- src/cykmatrix.h
+++ src/cykmatrix.h
@@ -1,10 +1,10 @@
 //**********************************************************************************
-// File		: cykmatrix.h
-// Purpose	: Declaration of classes for creation of CYK Parser Matrix
-// Author	: Vipin Gupta (viping@gmail.com), Manish Kushwaha (manish.kushwaha@gmail.com)
-// Revision	: 1.0	-	20-Feb-2005	Initial Draft
-//		: 2.0	-	03-Jun-2005	Minor Unpdation for compatibility with the downstream code (Manish Kushwaha)
-//		: 3.0	-	14-Jun-2005	Minor Unpdation for compatibility with the downstream code (Manish Kushwaha)
+// File         : cykmatrix.h
+// Purpose      : Declaration of classes for creation of CYK Parser Matrix
+// Author       : Vipin Gupta (viping@gmail.com), Manish Kushwaha (manish.kushwaha@gmail.com)
+// Revision     : 1.0   -       20-Feb-2005     Initial Draft
+//              : 2.0   -       03-Jun-2005     Minor Unpdation for compatibility with the downstream code (Manish Kushwaha)
+//              : 3.0   -       14-Jun-2005     Minor Unpdation for compatibility with the downstream code (Manish Kushwaha)
 //**********************************************************************************
 
 #ifndef _CYK_MATRIX__H_
@@ -27,20 +27,24 @@ using namespace std;
 
 
 
-enum TYPE_OF_LIST { _UNKNOWN = 0, _UPLIST = 1, _DOWNLIST = 2, _PARENTREF =3, _TERMRULE =4 };
+enum TYPE_OF_LIST
+{ _UNKNOWN = 0, _UPLIST = 1, _DOWNLIST = 2, _PARENTREF = 3, _TERMRULE = 4 };
 
 
 //**********************************************************************************
 //This class simply defines the coordinates of an element in the matrix cell's list
 //**********************************************************************************
 
-struct ElementCoordinates {
-	
-	int row,col,index;
-	TYPE_OF_LIST type;
-	
-	//Constructor
-	ElementCoordinates(): row(-1), col(-1), index(-1), type(_UNKNOWN) { }
+struct ElementCoordinates
+{
+
+    int row, col, index;
+    TYPE_OF_LIST type;
+
+    //Constructor
+      ElementCoordinates ():row (-1), col (-1), index (-1), type (_UNKNOWN)
+    {
+    }
 };
 
 class CykMatrixCell;
@@ -48,29 +52,36 @@ class CykMatrixCell;
 // This class defines one element of a matrix cell's Uplist or Downlist
 //**********************************************************************************
 
-class CykListElement {
+class CykListElement
+{
 //Friend Function required for direct member access to these functions
-friend void FillFRCellUList(CykMatrixCell &,char,int);
-friend bool FillFRCellDList(CykMatrixCell &,int);
-friend void FillRestCellProcess1(const vector<CykMatrixCell> &,CykMatrixCell &,int,int);
-friend bool FillRestCellProcess2(const vector<CykMatrixCell> &,CykMatrixCell &,int,int);
-friend int main();
-friend bool IsPossibleTree(const vector<CykMatrixCell> &);
-friend string BuildTrace(const vector<CykMatrixCell> &);
-friend void TraceFurther(ElementCoordinates,const vector<CykMatrixCell> &,ostringstream &);
-friend string make_parent(CykListElement temp);
+    friend void FillFRCellUList (CykMatrixCell &, char, int);
+    friend bool FillFRCellDList (CykMatrixCell &, int);
+    friend void FillRestCellProcess1 (const vector < CykMatrixCell > &,
+                                      CykMatrixCell &, int, int);
+    friend bool FillRestCellProcess2 (const vector < CykMatrixCell > &,
+                                      CykMatrixCell &, int, int);
+    friend int main ();
+    friend bool IsPossibleTree (const vector < CykMatrixCell > &);
+    friend string BuildTrace (const vector < CykMatrixCell > &);
+    friend void TraceFurther (ElementCoordinates,
+                              const vector < CykMatrixCell > &,
+                              ostringstream &);
+    friend string make_parent (CykListElement temp);
 
-private : string ruleLHS, ruleRHSpreDot, ruleRHSpostDot;
-		  ElementCoordinates coordinates;
-                  string Parents;
-				  int s1,s2,s3;
-				  int flag;
-		  double score;
+      private:string ruleLHS, ruleRHSpreDot, ruleRHSpostDot;
+    ElementCoordinates coordinates;
+    string Parents;
+    int s1, s2, s3;
+    int flag;
+    double score;
 
-public  : CykListElement() : score(MINUSINF) {}
+      public:CykListElement ():score (MINUSINF)
+    {
+    }
 
-bool operator==(const CykListElement&) const;
-bool operator!=(const CykListElement&) const;
+    bool operator== (const CykListElement &) const;
+    bool operator!= (const CykListElement &) const;
 
 };
 
@@ -79,47 +90,56 @@ bool operator!=(const CykListElement&) const;
 // This class defines one cell of the upper triangular CYK Matrix
 //**********************************************************************************
 
-typedef vector<CykListElement> ElementList;
-typedef vector<CykListElement>::iterator ElementListItr;
+typedef vector < CykListElement > ElementList;
+typedef vector < CykListElement >::iterator ElementListItr;
 
 
-class CykMatrixCell {
+class CykMatrixCell
+{
 //Friend Function required for direct member access to these functions
-friend void FillFRCellUList(CykMatrixCell &,char,int);
-friend bool FillFRCellDList(CykMatrixCell &,int);
-friend void FillRestCellProcess1(const vector<CykMatrixCell> &,CykMatrixCell &,int,int);
-friend bool FillRestCellProcess2(const vector<CykMatrixCell> &,CykMatrixCell &,int,int);
-friend int main();
-friend bool IsPossibleTree(const vector<CykMatrixCell> &);
-friend string BuildTrace(const vector<CykMatrixCell> &);
-friend void TraceFurther(ElementCoordinates,const vector<CykMatrixCell> &,ostringstream &);
+    friend void FillFRCellUList (CykMatrixCell &, char, int);
+    friend bool FillFRCellDList (CykMatrixCell &, int);
+    friend void FillRestCellProcess1 (const vector < CykMatrixCell > &,
+                                      CykMatrixCell &, int, int);
+    friend bool FillRestCellProcess2 (const vector < CykMatrixCell > &,
+                                      CykMatrixCell &, int, int);
+    friend int main ();
+    friend bool IsPossibleTree (const vector < CykMatrixCell > &);
+    friend string BuildTrace (const vector < CykMatrixCell > &);
+    friend void TraceFurther (ElementCoordinates,
+                              const vector < CykMatrixCell > &,
+                              ostringstream &);
 
-private : unsigned long UpListCount;
-		  unsigned long DownListCount;
-		  ElementList UpList;
-		  ElementListItr UpListItr;
-		  ElementList DownList;
-		  ElementListItr DownListItr;
+      private:unsigned long UpListCount;
+    unsigned long DownListCount;
+    ElementList UpList;
+    ElementListItr UpListItr;
+    ElementList DownList;
+    ElementListItr DownListItr;
 
-public	: CykMatrixCell() : UpListCount(0), DownListCount(0), UpListItr(UpList.begin()), DownListItr(DownList.begin()) {}
-		  
+      public:CykMatrixCell ():UpListCount (0), DownListCount (0),
+        UpListItr (UpList.begin ()), DownListItr (DownList.begin ())
+    {
+    }
+
 };
 
 class cartesian1
 {
-	private :	string str;
+    private:string str;
     ElementCoordinates parent3;
-	string parent1,parent2;
-	double scoretemp;
-friend	int chek(vector<cartesian1> ss,cartesian1 strtemp);
-friend void FillRestCellProcess1(const vector<CykMatrixCell> &mat,CykMatrixCell &cykmc,int i,int j);
+    string parent1, parent2;
+    double scoretemp;
+    friend int chek (vector < cartesian1 > ss, cartesian1 strtemp);
+    friend void FillRestCellProcess1 (const vector < CykMatrixCell > &mat,
+                                      CykMatrixCell & cykmc, int i, int j);
 
 
-	public:
-cartesian1()
-{};
+  public:
+      cartesian1 ()
+    {
+    };
 
 };
 
 #endif
-
