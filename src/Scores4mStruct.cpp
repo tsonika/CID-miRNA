93a318b940145cf3009339a4615494d516830cc1
Alejandro Dubrovsky <alito@organicrobot.com>
2016-04-09 23:33:32 +1000

    Put the calculating of the score into a function.

--- src/Scores4mStruct.cpp
+++ src/Scores4mStruct.cpp
@@ -21,6 +21,27 @@ using namespace std;
 char matrix[5][81];             //This matrix represents the screen of 5x80 (Height x Width) characters
 
 
+double score_mirna(string& miRNAstruct) {
+    miRNAstats structstats = getstats (miRNAstruct);
+    double structscore = 0;
+
+    structscore = structstats.n_base_pairs * 2;     //(Number of paired bases)*(+1)
+
+    for (size_t x = 0; x < structstats.sym_len.size (); x++)
+        if (structstats.sym_len[x] > 2)
+            structscore += (structstats.sym_len[x] * 2) * (-2);     //(Number of unpaired bases in SYM of size>2)*(-2)
+        else
+            structscore += structstats.sym_len[x];  //(Number of unpaired bases in SYM of size<=2)*(+0.5)
+
+    for (size_t x = 0; x < structstats.asym_nbases.size (); x++)
+        structscore += structstats.asym_nbases[x] * (-2);   //(Number of bases in ASYM)*(-2)
+
+    if (structstats.n_loop_bases > 5)
+        structscore += (5 - structstats.n_loop_bases);      //(Number of bases in terminal loop of size >5)*(-1)
+
+    return structscore;
+}
+
 int main (int argc, char *argv[])
 {
 //Recieve input from command line
@@ -35,7 +56,7 @@ int main (int argc, char *argv[])
     char *InFileName = argv[1];
     char *OutFileName = argv[2];
 
-//Open input and output files for reading
+    //Open input and output files for reading
     ifstream *infile;
     infile = new ifstream;
     infile->open (InFileName, ios::in);
@@ -52,8 +73,7 @@ int main (int argc, char *argv[])
     }
 
     string inpline, inpline2, inpline3, miRNAstruct;
-    bool tracking = false, first_time = true;
-    miRNAstats structstats;
+    bool tracking = false;
 
     while (!infile->eof ()) {
         //Run through the entire input file
@@ -68,53 +88,13 @@ int main (int argc, char *argv[])
 
         if (inpline2.find (">") == 0) { //miRNA name line
             if (tracking) {     //End and write an miRNA structure
-                structstats = getstats (miRNAstruct);
                 *outfile << miRNAstruct;        //This line prints the original structure to the output file
-
-                *outfile << "SCORE: ";
-                double structscore = 0;
-
-                structscore = structstats.n_base_pairs * 2;     //(Number of paired bases)*(+1)
-
-                for (int x = 0; x < structstats.sym_len.size (); x++)
-                    if (structstats.sym_len[x] > 2)
-                        structscore += (structstats.sym_len[x] * 2) * (-2);     //(Number of unpaired bases in SYM of size>2)*(-2)
-                    else
-                        structscore += structstats.sym_len[x];  //(Number of unpaired bases in SYM of size<=2)*(+0.5)
-
-                for (int x = 0; x < structstats.asym_nbases.size (); x++)
-                    structscore += structstats.asym_nbases[x] * (-2);   //(Number of bases in ASYM)*(-2)
-
-                if (structstats.n_loop_bases > 5)
-                    structscore += (5 - structstats.n_loop_bases);      //(Number of bases in terminal loop of size >5)*(-1)
-
-                *outfile << structscore << "\n";
-
-/*         *outfile<<"Number of Continuous Stems :"<<structstats.n_stems<<"\n";
-
-         for (int x=0; x < structstats.stem_len.size(); x++ )
-             *outfile<<"\tSTEM "<<x+1<<" length: "<<structstats.stem_len[x]<<endl;
-
-           *outfile<<"\nNumber of Asymmetric Bulges :"<<structstats.n_asyms<<"\n";
-                                                                                                                             
-         for (int x=0; x < structstats.asym_len.size(); x++ )
-             *outfile<<"\tASYM "<<x+1<<" length: "<<structstats.asym_len[x]<<"\t("<<structstats.asym_nbases[x]<<")\n";
-                                                                                                                             
-           *outfile<<"\nNumber of Symmetric Bulges :"<<structstats.n_syms<<"\n";
-                                                                                                                             
-         for (int x=0; x < structstats.sym_len.size(); x++ )
-             *outfile<<"\tSYM "<<x+1<<" length: "<<structstats.sym_len[x]<<endl;
-                                                                                                                             
-           *outfile<<"\nNumber of Bases :"<<structstats.n_bases;
-           *outfile<<"\nNumber of Base Pairs :"<<structstats.n_base_pairs;
-           *outfile<<"\nNumber of Bases in the Terminal Loop :"<<structstats.n_loop_bases<<"\n\n"; */
-
-            }                   //End- End and write an miRNA structure //Initiate an miRNA structure
+                *outfile << "SCORE: " << score_mirna(miRNAstruct) << "\n";    
+            }                   
 
             //Initiate an miRNA structure
             miRNAstruct = "";
             tracking = true;
-            first_time = true;
             *outfile << inpline2 << endl;
             getline (*infile, inpline3);
             *outfile << inpline3 << endl;
@@ -124,51 +104,12 @@ int main (int argc, char *argv[])
         }
     }                           //End- Run through the entire input file - while
 
-//Write the last miRNA structure
+    //Write the last miRNA structure
     if (miRNAstruct.length () != 0) {
-        structstats = getstats (miRNAstruct);
         *outfile << miRNAstruct;        //This line prints the original structure to the output file
-
-        *outfile << "SCORE: ";
-        double structscore = 0;
-
-        structscore = structstats.n_base_pairs * 2;     //(Number of paired bases)*(+1)
-
-        for (int x = 0; x < structstats.sym_len.size (); x++)
-            if (structstats.sym_len[x] > 2)
-                structscore += (structstats.sym_len[x] * 2) * (-2);     //(Number of unpaired bases in SYM of size>2)*(-2)
-            else
-                structscore += structstats.sym_len[x];  //(Number of unpaired bases in SYM of size<=2)*(+0.5)
-
-        for (int x = 0; x < structstats.asym_nbases.size (); x++)
-            structscore += structstats.asym_nbases[x] * (-2);   //(Number of bases in ASYM)*(-2)
-
-        if (structstats.n_loop_bases > 5)
-            structscore += (5 - structstats.n_loop_bases);      //(Number of bases in terminal loop of size >5)*(-1)
-
-        *outfile << structscore << "\n";
-
-/* *outfile<<"Number of Continuous Stems :"<<structstats.n_stems<<"\n";
-
-         for (int x=0; x < structstats.stem_len.size(); x++ )
-             *outfile<<"\tSTEM "<<x+1<<" length: "<<structstats.stem_len[x]<<endl;
-                                                                                                                             
-           *outfile<<"\nNumber of Asymmetric Bulges :"<<structstats.n_asyms<<"\n";
-                                                                                                                             
-         for (int x=0; x < structstats.asym_len.size(); x++ )
-             *outfile<<"\tASYM "<<x+1<<" length: "<<structstats.asym_len[x]<<"\t("<<structstats.asym_nbases[x]<<")\n";
-                                                                                                                             
-           *outfile<<"\nNumber of Symmetric Bulges :"<<structstats.n_syms<<"\n";
-                                                                                                                             
-         for (int x=0; x < structstats.sym_len.size(); x++ )
-             *outfile<<"\tSYM "<<x+1<<" length: "<<structstats.sym_len[x]<<endl;
-
-   *outfile<<"\nNumber of Bases :"<<structstats.n_bases;
-   *outfile<<"\nNumber of Base Pairs :"<<structstats.n_base_pairs;
-   *outfile<<"\nNumber of Bases in the Terminal Loop :"<<structstats.n_loop_bases<<"\n\n"; */
-
+        *outfile << "SCORE: " << score_mirna(miRNAstruct) << "\n";    
     }
-//End- Write the last miRNA structure
+
 
     outfile->close ();
     infile->close ();
